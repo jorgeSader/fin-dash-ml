51a15f7959c12e1f0111c330d3ddc18eea7d9230
Jorge Sader <Jorge.Sader@gmail.com>
2023-03-26 18:05:44 -0400

    Client -  setup, MUI & TS config,  and navigatiom

---
+++ client/src/App.tsx
@@ -0,0 +1,32 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { Box } from '@mui/system';
+import { useMemo } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { themeSettings } from './themes';
+import NavBar from '@/components/NavBar';
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings), []);
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<div>Dashoboard Page</div>} />
+              <Route
+                path="/predictions"
+                element={<div>Predictions Page</div>}
+              />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
